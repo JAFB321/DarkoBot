ca0b2839829d9827e99fa4c2d5b3a2876f983fdf
FLANAX550 <jafb321@gmail.com>
2020-10-11 01:03:52 -0700

    ADD Modulo dotenv, fixes

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
+require('dotenv').config();
+
 const { Client } = require('discord.js');
 const bot = new Client();
 
 bot.on('ready', () => {
   console.log(`Bot is ready as ${bot.user.tag}`);
-  bot.user.setStatus('idle');
-
 });
 
 bot.on('message', (message) => {
 
     const mensaje = message.content;
     if (mensaje.includes('ahhh')) {
-      message.channel.send('me falta el aire');
-      message.channel.send('y el corazon tucum tucum tucum');
+      message.channel.send('/tts me falta el aire');
+      message.channel.send('y el corazon tucum tucum tucum');      
     }
 
-    if(mensaje.includes('adios')){
-      bot.user.setStatus('idle');
+    if(mensaje.includes('mi avatar')){
+      message.reply(message.author.displayAvatarURL());
     }
 
-    if(mensaje.includes('hola')){
-      bot.user.setStatus('online');
-    }
+    
 
 });
-bot.login('NzMwNTQzOTE0MzU1OTE2ODcx.XwZCCg.fsihaiG49zRrwLeBhYOO7cAyM_s');
+bot.login(process.env.BotToken);
