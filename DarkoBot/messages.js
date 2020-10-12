9e736e41c3e47a5d0141beb3411405bff053b6a2
FLANAX550 <jafb321@gmail.com>
2020-10-12 00:21:45 -0700

    fix index.js, fix messages.js

--- DarkoBot/messages.js
+++ DarkoBot/messages.js
@@ -5,16 +5,32 @@ exports.addEvents = (bot) => {
     bot.on('message', (message) => {
 
         const msg = message.content;
-        if (msg.includes('ahhh')) {
+        const menciones = message.mentions.users;
+        const [_prefix = '', command = '', ...params] = msg.split(' ');
+
+        if (_prefix !== prefix) return;
+
+
+        if (command === 'ahh') {
             message.channel.send('me falta el aire');
             message.channel.send('y el corazon tucum tucum tucum');
         }
 
-        if (msg.includes('mi avatar')) {
-            message.reply(message.author.displayAvatarURL());
+        if (command === 'avatar') {
+            
+            if(menciones.size > 0){
+                menciones.each(user => {
+                    message.channel.send(user.displayAvatarURL());
+                });                
+            }
+            else{
+                message.channel.send(message.author.displayAvatarURL());
+            }
+
+            
         }
 
-        if (msg.includes(`${prefix} que coma verga`)) {
+        if (command === 'hate') {
             let mention = message.mentions.users.first();
             let name = mention.username;
 
--- index.js
+++ index.js
@@ -2,7 +2,3 @@ require('dotenv').config();
 
 const DarkoBot = require('./DarkoBot/DarkoBot');
 DarkoBot.StartBot(process.env.DarkoBotToken);
-
-
-
-
