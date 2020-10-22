25b6a3e1620b265d967dc16b40901ee0a80d2480
FLANAX550 <jafb321@gmail.com>
2020-10-22 15:29:28 -0700

    Implement an express server, node packs. 'colors' and 'express'

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 require('dotenv').config();
+const express = require('express');
+const colors = require('colors');
 
-const DarkoBot = require('./DarkoBot/DarkoBot');
-DarkoBot.StartBot(process.env.DarkoBotToken);
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+
+    const DarkoBot = require('./DarkoBot/DarkoBot');
+    DarkoBot.StartBot(process.env.DarkoBotToken);
+
+    console.log(`Server on port ${app.get('port')}`.blue);
+});
+
+app.get('/', (req, res) => {
+    res.send('DarkoBot Online');    
+});
+
