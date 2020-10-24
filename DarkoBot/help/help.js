f4dc3971605b74fa1637b83ddb01212ce1001f76
FLANAX550 <jafb321@gmail.com>
2020-10-24 02:11:57 -0700

    Add missing files for the last commit

---
+++ DarkoBot/help/help.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require('discord.js');
+const { prefix, GetParams } = require('../botconfig');
+
+let help_schemas = [];
+
+exports.setHelpSchemas = (schemas) => {
+    help_schemas = schemas;
+}
+
+exports.addEvents = (bot) => {
+
+    bot.on('message', (message) => {
+
+        const msg = message.content;
+        const menciones = message.mentions.users;
+
+        const { _prefix, command, params, validCommand } = GetParams(msg);
+
+        if(!validCommand || command !== 'help') return;
+
+        let res; 
+
+        if (params.length === 0) {
+            res = new MessageEmbed();
+            
+            help_schemas.forEach((schema) => {
+                
+            });
+
+        }
+
+
+    });
+
+}
