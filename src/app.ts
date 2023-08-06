d602482a6184fa2f31b8eb47882b2632e377d15f
jafb321 <jafb321@gmail.com>
2023-08-06 16:54:51 -0700

    feat(app): setup http server & reset timeout sleep

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import type { DiscordClient } from "./types/discord";
-import { BOT_TOKEN } from "./config";
 import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { APP_URL, BOT_TOKEN, PORT, RENDER_TIMEOUT_SLEEP } from "./config";
+import fetch from "node-fetch";
+import express from "express";
 import { commands } from "./commands";
 import { events } from "./events";
 
@@ -33,3 +35,23 @@ for (const event of events) {
 
 // Log in to Discord
 client.login(BOT_TOKEN);
+
+// Set up express server
+const app = express();
+app.listen(PORT, () => console.log(`Http server running on port ${PORT}`))
+
+app.get('/reset-timeout', (req, res) => {
+  console.log('Timeout reset');
+  res.send('Darkobot is running');
+})
+
+// Avoid free hosting sleep
+async function AutoCallLoop() {
+  if (APP_URL) {
+		await fetch(APP_URL+'/reset-timeout').catch(console.error);
+	}
+	setTimeout(() => {
+		AutoCallLoop();
+	}, RENDER_TIMEOUT_SLEEP*1000);
+}
+AutoCallLoop();
